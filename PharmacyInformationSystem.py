f56eba95c2d7bf2b804870bb3b701f1717794068
christianebacani <121742592+christianebacani@users.noreply.github.com>
2024-05-16 09:44:32 +0800

    Update PharmacyInformationSystem.py

--- PharmacyInformationSystem.py
+++ PharmacyInformationSystem.py
@@ -10,11 +10,21 @@ class Pharmacy:
             print("'" + productName + "' already added to the inventory")
 
     def displayProduct(self):
-        print("\nCURRENT INVENTORY")
-
+        print("\tCURRENT INVENTORY")
         for product, details in self.inventory.items():
-            print(f"\nProduct : {product} \nPrice : P {details["Price"]} \nQuantity : {details["Quantity"]}\n")
+            print(f"\nProduct : {product} \nPrice : P {details['Price']} \nQuantity : {details['Quantity']}\n")
+        print("\n\n")
 
+    def removeProduct(self, remove_product):
+        if remove_product in self.inventory:
+            del self.inventory[remove_product]
+            print(f"{remove_product} is Successfully Removed!")
+
+        elif remove_product == "x".lower():
+            exit()
+
+        else:
+            print(f"{remove_product} not found in the inventory")
 
 pharmacySystem = Pharmacy()
 
@@ -28,16 +38,10 @@ while True:
 
     pharmacySystem.addProduct(productName, price, quantity)
 
-    userAddAgain = str(input("Do you want to add more products to the inventory (y/n?: "))
-
-    if userAddAgain != "y":
+    userAddAgain = str(input("Do you want to add more products to the inventory (y/n)?: "))
+    if userAddAgain.lower() != "y":
         break
 
+remove_product = str(input("What product you want to remove ? press 'x' to exit : "))
+pharmacySystem.removeProduct(remove_product)
 pharmacySystem.displayProduct()
-
-        
-
-
-
-
-
